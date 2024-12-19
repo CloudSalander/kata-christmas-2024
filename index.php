17368a70420250f325655d14b6ef22d2be5a41ef
CloudSalander <ruben.as532@gmail.com>
2024-12-19 10:40:42 +0100

    Base Case- 1 Row

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
 <?php
-
+define("LEAF","*");
+//TODO: Input validation could be great. Is a number, is positive?
 $height = intval(readline());
-var_dump($height);
+$con = 0;
+$n_leafs = 1;
+
+while($con < $height) {
+    printLeafsRow($n_leafs);
+    echo PHP_EOL;
+    ++$con;
+    $n_leafs += 2;
+}
+printTrunk($n_leafs);
+
+function printLeafsRow(int $n_leafs): void {
+    $con = 0;
+    while($con < 1) {
+        echo " ".LEAF." ";
+        ++$con;
+    }
+}
+
+function printTrunk(int $tree_length): void {
+    echo "|||";
+}
 
 ?>
