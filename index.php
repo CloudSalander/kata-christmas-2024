9adb71fb77444dae5b3b28dca49b232a43766309
CloudSalander <ruben.as532@gmail.com>
2024-12-19 10:47:09 +0100

    What I did in 30 minutes

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <?php
 define("LEAF","*");
+define("TRUNK","|||");
 //TODO: Input validation could be great. Is a number, is positive?
 $height = intval(readline());
 $con = 0;
 $n_leafs = 1;
+$tree_length = 1 + (2*$height);
 
 while($con < $height) {
-    printLeafsRow($n_leafs);
+    printLeafsRow($n_leafs,$tree_length);
     echo PHP_EOL;
     ++$con;
     $n_leafs += 2;
 }
 printTrunk($n_leafs);
 
-function printLeafsRow(int $n_leafs): void {
+function printLeafsRow(int $n_leafs, int $tree_length): void {
     $con = 0;
     while($con < 1) {
         echo " ".LEAF." ";
@@ -22,7 +24,7 @@ function printLeafsRow(int $n_leafs): void {
 }
 
 function printTrunk(int $tree_length): void {
-    echo "|||";
+    echo TRUNK;
 }
 
 ?>
