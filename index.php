c447cad03349cc84502b73be786e75563a80a3a9
CloudSalander <ruben.as532@gmail.com>
2025-01-14 09:27:39 +0100

    Print Max Length Leafs Row

--- index.php
+++ index.php
@@ -3,6 +3,15 @@ define("LEAF","*");
 define("BLANK_LEAF"," ");
 define("TRUNK","|||");
 
+$treeHeight = intval(readline());
+$treeLength = $treeHeight*2-1;
+
+printLeafsRow($treeLength);
+//printTrunk($treeLength);
+
+function printLeafsRow(int $rowLength): void {
+    echo str_repeat(LEAF,$rowLength);
+}
 
 
 ?>
