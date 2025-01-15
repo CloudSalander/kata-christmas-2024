5f434f4dd9cfaab0f616a53d7de73976325e51a8
CloudSalander <ruben.as532@gmail.com>
2025-01-15 10:24:23 +0100

    First complete solution

--- index.php
+++ index.php
@@ -5,15 +5,22 @@ define("TRUNK","|||");
 define("TRUNK_LENGTH",3);
 
 $treeHeight = intval(readline());
-$treeLength = $treeHeight*2-1;
-printLeafsRow($treeLength);
-printTrunk($treeLength);
+printTree($treeHeight);
 
+function printTree(int $treeHeight): void {
+    $treeLength = $treeHeight*2-1;
+    $rowLength = 1;
+    for($i = 0; $i < $treeHeight; ++$i) { 
+        printLeafsRow($rowLength,$treeLength);
+        $rowLength += 2;
+    }
+    printTrunk($treeLength);
+}
 
 function printTrunk(int $treeLength): void {
     if($treeLength == 1) echo TRUNK;
     else {
-        $padding = ($treeLength-TRUNK_LENGTH)/2;
+        $padding = intdiv($treeLength-TRUNK_LENGTH,2);
         echo str_repeat(BLANK_LEAF,$padding);
         print(TRUNK);
         echo str_repeat(BLANK_LEAF,$padding);
@@ -21,8 +28,14 @@ function printTrunk(int $treeLength): void {
     echo PHP_EOL;
 }
 
-function printLeafsRow(int $rowLength): void {
-    echo str_repeat(LEAF,$rowLength);
+function printLeafsRow(int $rowLength, int $treeLength): void {
+    if($treeLength == 1) echo BLANK_LEAF.LEAF.BLANK_LEAF;   
+    else  {
+        $padding = intdiv(($treeLength-$rowLength),2);
+        if($padding > 0) echo str_repeat(BLANK_LEAF,$padding);
+        echo str_repeat(LEAF,$rowLength);
+        if($padding > 0) echo str_repeat(BLANK_LEAF,$padding);
+    }
     echo PHP_EOL;
 }
 
